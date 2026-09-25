625976ea70b9c11722cd78fe08dc6aeab40707c4
DEBASIS PAUL <unkowndp12@gmail.com>
2026-09-25 11:42:48 +0530

    Refactor K-means clustering with new data and parameters
    Updated data source and adjusted K-means parameters. Fixed centroid calculation and improved clustering logic.

--- K_mean(sort).py
+++ K_mean(sort).py
@@ -2,44 +2,30 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("driver.csv")
+data = pd.read_csv"KMeans_Data.csv")
 
-x = data[["mean_dist_day", "mean_over_speed_perc"]].values
+x = data[["X", "Y"]].values
 
-k = 2
-centroids = x[:k].copy()
-
+k = 3
+centroids = x[:k].
 for i in range(5):
-
     clusters = [[] for j in range(k)]
-
     for point in x:
         d = []
-
         for c in centroids:
             d.append(np.sqrt(np.sum((point - c) ** 2)))
-
         cluster = np.argmin(d)
         clusters[cluster].append(point)
-
     new_centroids = []
-
     for j in range(k):
         new_centroids.append(np.mean(clusters[j], axis=0))
-
-    new_centroids = np.array(new_centroids)
-
-    if np.allclose(centroids, new_centroids):
-        break
-
-    centroids = new_centroids
+    centroids=np.array(new_centroides)
 
 print("Centroids:")
 print(centroids)
 
 for i in range(k):
     print("Cluster", i + 1, ":", len(clusters[i]), "drivers")
-
 
 # Graph
 
